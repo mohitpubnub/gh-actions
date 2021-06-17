9b6a50f66d12e969692bc45bfe4430fc693609c3
Mohit Tejani <mohit.tejani@pubnub.com>
2021-06-17 18:06:46 +0530

    test_app and test script

---
+++ test_driver/app_test.dart
@@ -0,0 +1,45 @@
+// Imports the Flutter Driver API.
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('test_app', () {
+    final counterTextFinder = find.byValueKey('counter');
+    final buttonFinder = find.byValueKey('increment');
+
+    late FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      driver.close();
+    });
+
+    test('starts at 0', () async {
+      // Use the `driver.getText` method to verify the counter starts at 0.
+      expect(await driver.getText(counterTextFinder), "0");
+    });
+
+    test('increments the counter', () async {
+      // First, tap the button.
+      await driver.tap(buttonFinder);
+
+      // Then, verify the counter text is incremented by 1.
+      expect(await driver.getText(counterTextFinder), "1");
+    });
+
+    test('increments the counter during animation', () async {
+      await driver.runUnsynchronized(() async {
+        // First, tap the button.
+        await driver.tap(buttonFinder);
+
+        // Then, verify the counter text is incremented by 1.
+        expect(await driver.getText(counterTextFinder), "1");
+      });
+    });
+  });
+}
